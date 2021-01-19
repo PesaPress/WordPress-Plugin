419b7b1ef9651457b15f0fac0305d1e55b8c91e5
Paul Kevin <paultitude@gmail.com>
2021-01-19 11:39:01 +0300

    Action on integration page for more information

--- app/views/admin/settings/class-pp-views-admin-settings-integration.php
+++ app/views/admin/settings/class-pp-views-admin-settings-integration.php
@@ -19,7 +19,7 @@ class PP_Views_Admin_Settings_Integration extends PP_Core_View {
 		<article class="uk-article">
 			<h1 class="uk-article-title"><?php _e( 'Payment Form Shortcode', 'pesapress' ); ?></h1>
 			<p class="uk-text-lead uk-text-muted">[pesapress_form]</p>
-			<p><?php _e( 'Use this shortcode to display the payment form on the page. Attributes include :', 'pesapress' ); ?></p>
+			<p><?php _e( 'Use this shortcode to display the payment form on the page. Attributes include ', 'pesapress' ); ?>:</p>
 			<ul>
 				<li><span class="uk-text-bold">button_name</span> - <span class="uk-text-muted"><?php _e( 'the name of the button. Defaults to "Complete Payment"', 'pesapress' ); ?></span></li>
 				<li><span class="uk-text-bold">total_label</span> - <span class="uk-text-muted"><?php _e( 'the text used as total. Defaults to "Total"', 'pesapress' ); ?></span></li>
@@ -29,10 +29,11 @@ class PP_Views_Admin_Settings_Integration extends PP_Core_View {
 				<li><span class="uk-text-bold">ajax_form</span> - <span class="uk-text-muted"><?php _e( 'submit the form using ajax or full page post. Defaults to "true"', 'pesapress' ); ?></span></li>
 				<li><span class="uk-text-bold">show_amount</span> - <span class="uk-text-muted"><?php _e( 'shot the amount on the payment form. This allows a user to input any value to pay "false"', 'pesapress' ); ?></span></li>
 			</ul>
-			<p><?php _e( 'Example usage : ', 'pesapress' ); ?></p>
+			<p><?php _e( 'Example usage ', 'pesapress' ); ?> :</p>
 			[pesapress_form button_name="Pay Me" amount="100" gateway_id="1"]
 		</article>
 		<?php
+		do_action( 'pesapress_integration_instructions' );
 		$content = ob_get_clean();
 		return apply_filters( 'pesapress_view_admin_integration_settings', $content );
 	}
